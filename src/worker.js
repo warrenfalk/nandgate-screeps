1c5e878b4e9feffdccf6c0376968bbd99cea55d2
Warren Falk <warren@warrenfalk.com>
2016-12-05 19:57:19 -0500

    Update

--- src/worker.js
+++ src/worker.js
@@ -11,7 +11,33 @@ const WorkerSector = {
 
 	},
 	employ: function(creep) {
-
+        let room = creep.room;
+        let goal = creep.memory.goal;
+        let complete = !goal;
+        switch (goal) {
+            case 'harvest':
+                complete = harvestGoal.isComplete(creep);
+                break;
+            case 'supply':
+                complete = supplyGoal.isComplete(creep);
+                break;
+            case 'build':
+                complete = buildGoal.isComplete(creep);
+                break;
+            case 'upgrade':
+                complete = upgradeGoal.isComplete(creep);
+                break;
+            case 'tower':
+                complete = towerGoal.isComplete(creep);
+                break;
+            case 'manual':
+                complete = false;
+                break;
+        }
+        if (complete) {
+            goal = findJob(creep, room);
+            creep.newGoal = goal;
+        }
 	}
 }
 
