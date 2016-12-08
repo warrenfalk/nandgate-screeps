e00c404cd89b49e9445a6c41714bd72d3fb22f25
Warren Falk <warren@warrenfalk.com>
2016-12-07 22:00:24 -0500

    Update

--- src/quarry.js
+++ src/quarry.js
@@ -175,7 +175,7 @@ Quarry.prototype.employMiner = function(creep) {
     }
 }
 function getRoomCoords(roomName) {
-    let match = /^[EW][0-9]+[NS][0-9]+$/.exec(roomName);
+    let match = /^([EW])([0-9]+)([NS])([0-9]+)$/.exec(roomName);
     return {
         x: (match[2]|0) * (match[1] === 'E' ? 1 : -1),
         y: (match[4]|0) * (match[3] === 'S' ? 1 : -1),
