14d69a139d5560184be0e0a33d411e819d98c64e
Warren Falk <warren@warrenfalk.com>
2016-12-08 14:00:05 -0500

    Update

--- src/quarry.js
+++ src/quarry.js
@@ -490,7 +490,7 @@ const QuarrySector = {
                     let dest = new RoomPosition(d.x, d.y, d.roomName);
                     // if there is already a carrier there, give him our stuff
                     let dcreep = dest.lookFor(LOOK_CREEPS);
-                    let capacity = dcreep && dcreep.quarry && dcreep.carryCapacity - dcreep.carry.energy
+                    let capacity = dcreep && dcreep.quarry && (dcreep.carryCapacity - dcreep.carry.energy)
                     console.log(creep.name, "dcreep", dcreep, capacity);
                     if (capacity) {
                         let amount = Math.min(carry, capacity);
